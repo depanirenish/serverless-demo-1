65d3393570d8abcfb8c073d346feeb389168321b
renishdepani <depanirenish@gmail.com>
2023-11-02 17:20:58 +0530

    serverless configuration

---
+++ aws-python-http-api-project/handler.py
@@ -0,0 +1,12 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "This is github action!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
