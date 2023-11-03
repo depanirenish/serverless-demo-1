dc5c128d13a350dca2dc5bbc485f495cbd191ff9
renishdepani <depanirenish@gmail.com>
2023-11-03 14:12:27 +0530

    changed handler file

--- handler.py
+++ handler.py
@@ -3,8 +3,8 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "This is github action!!-4",
-        "input": event,
+        "message": "This is github action!!-5",
+        "custom_heading": "this is value of custom heading",
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
